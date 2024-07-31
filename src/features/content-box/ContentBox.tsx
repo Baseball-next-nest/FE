9f91005c0fd3f1ef176da0297a4deb052e5a282d
robbins312 <robbins312@naver.com>
2024-07-31 22:34:37 +0900

    create contentbox

---
+++ src/features/content-box/ContentBox.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactNode } from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+interface PlayerGridTableProps {
+  children: ReactNode;
+}
+export const ContentBox: FC<PlayerGridTableProps> = ({ children }) => {
+  return (
+    <div className="border border-[#e2e5e8] overflow-hidden relative mx-[9px] my-[9px]  bg-white">
+      {children}
+    </div>
+  );
+};
