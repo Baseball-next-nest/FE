e9996b2012697010cdec0bd9cea6f2d1a31bdb8c
robbins312 <robbins312@naver.com>
2024-12-18 10:38:05 +0900

    api fetcher 수정 및 fet&upload 추가

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,22 +1,24 @@
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://43.201.105.90:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://13.209.70.240:3000";
 
 async function fetcher(url: string, options?: RequestInit) {
+  const { headers: customHeaders, ...restOptions } = options || {};
+
+  // Content-Type 자동 설정: JSON일 경우만 지정
+  const isJSON = customHeaders?.["Content-Type"] === "application/json";
+
   const res = await fetch(`${BASE_URL}${url}`, {
-    ...options,
+    ...restOptions,
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      // "Access-Control-Allow-Methods": "'GET, POST, PUT, DELETE'",
-      // "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      ...options?.headers,
+      ...(isJSON ? { "Content-Type": "application/json" } : {}), // JSON만 Content-Type 설정
+      ...customHeaders, // 추가적인 사용자 정의 헤더
     },
     cache: "no-store",
-    // mode: "cors",
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    const errorText = await res.text(); // 에러 메시지 읽기
+    throw new Error(`Failed to fetch data: ${errorText}`);
   }
 
   return res.json();
@@ -37,8 +39,27 @@ export async function getPlayerNameData(name: string) {
 }
 
 export async function createPost(postData: any) {
-  return fetcher(`/player/pitcher`, {
+  // console.log("post!!! " + JSON.stringify(postData));
+  return fetcher(`/community/create`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(postData),
   });
+}
+
+export async function uploadFiles(file: any) {
+  return fetcher(`/community/uploadFile`, {
+    method: "POST",
+    body: file,
+  });
+}
+
+export async function fetchSectionPosts(section: string) {
+  return fetcher(`/community?section=${section}`);
+}
+
+export async function fetchBoardPostById(id: number) {
+  return fetcher(`/community/one?id=${id}`);
 }
